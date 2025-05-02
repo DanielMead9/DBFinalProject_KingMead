883897868eea86e905940c15581be37550c7e213
Daniel Mead <157059165+DanielMead9@users.noreply.github.com>
2025-05-02 13:39:31 -0400

    Add files via upload

---
+++ DigitialInventory.sql
@@ -0,0 +1,24 @@
+Create database DigitialInvetory;
+
+use DigitialInvetory;
+
+Create table if not exists Product(
+ProductName varchar(50) not null,
+ProductType varchar(20) not null,
+SellPrice double(5,2) not null,
+CostPrice double(5,2) not null,
+AisleNum int,
+AmountStorage int,
+AmountShelf int,
+Primary Key (ProductName));
+
+Create table if not exists Supplier(
+SupplierName varchar(50) not null,
+SupplierLocation varchar(50),
+Primary Key (SupplierName));
+
+Create Table if not exists SupplierMapping(
+SupplierName varchar(50),
+ProductName varchar(50),
+Foreign Key (SupplierName) references Supplier(SupplierName),
+Foreign Key (ProductName) references Product(ProductName));

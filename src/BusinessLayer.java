e66ff7fdc62d25568c56ce9a43daed2b0fbdd786
DanielMead9 <meadd@merrimack.edu>
2025-05-08 18:39:59 -0400

    bug

--- src/BusinessLayer.java
+++ src/BusinessLayer.java
@@ -82,12 +82,13 @@ public class BusinessLayer {
             return;
         }
         ArrayList<Double> temp = new ArrayList<>();
-        card += temp.get(0);
-        profit += temp.get(1);
-        total = temp.get(2);
 
         // Process the large order
         dal.processLargeOrder(userName, password, buyerName, location, phone, email, items, quantities, temp);
+
+        card += temp.get(0);
+        profit += temp.get(1);
+        total = temp.get(2);
 
         System.out.println("Order has been successfully processed for: " + buyerName);
     }
